b11bb52eb70491d7e28b61af8aa10a00d300e066
Jesus Vazquez <vapa.jesusangel@gmail.com>
2024-10-06 12:15:21 -0400

    Create ParticlesBackground.jsx

---
+++ src/components/ParticlesBackground.jsx
@@ -0,0 +1,55 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+
+const ParticlesBackground = () => {
+  const particlesInit = async (main) => {
+    // Inicializa el motor completo
+    await loadFull(main);
+  };
+
+  const particleOptions = {
+    background: {
+      color: {
+        value: "#000000", // Fondo negro
+      },
+    },
+    fpsLimit: 60,
+    particles: {
+      number: {
+        value: 100,
+        density: {
+          enable: true,
+          value_area: 800,
+        },
+      },
+      color: {
+        value: "#ffffff", // Partículas blancas
+      },
+      shape: {
+        type: "circle",
+      },
+      opacity: {
+        value: 0.5,
+      },
+      size: {
+        value: 3,
+      },
+      move: {
+        enable: true,
+        speed: 1,
+        direction: "none",
+        outModes: {
+          default: "bounce",
+        },
+      },
+    },
+  };
+
+  return (
+    <Particles id="tsparticles" init={particlesInit} options={particleOptions} />
+  );
+};
+
+export default ParticlesBackground;
