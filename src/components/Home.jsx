34be209888a42f840be6f1aa0902369f4c5ad5d1
Jesus Vazquez <vapa.jesusangel@gmail.com>
2024-10-06 14:28:19 -0400

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,18 @@
 // eslint-disable-next-line no-unused-vars
-import React from "react";
+import React, {useRef, useEffect} from "react";
 import ParticlesBackground from "./ParticlesBackground";
 import "../css/Home.css"; // Importa el CSS
+import { typeWriterEffect } from './typeWriter.js';
+
 
 const Home = () => {
+
+  const textRef = useRef(null);
+
+  useEffect(() => {
+    typeWriterEffect('animated-text', 70); 
+  }, []);
+
   return (
     <div>
       {/* Particles Background */}
@@ -15,6 +24,7 @@ const Home = () => {
           <div className="hero-content">
             <h1 className="display-3 text-white">Angel Vazquez</h1>
             <p className="lead text-light">Software Developer</p>
+            <span id="animated-text" ref={textRef}>Give me a foothold and I will move the world.</span>
             <div className="mt-4">
               <a href="#portfolio" className="btn btn-primary btn-lg me-3">
                 View My Work
