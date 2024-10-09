f0eaeba19f3c3eb4a0a6d95e2e24456807a9a485
Jesus Vazquez <vapa.jesusangel@gmail.com>
2024-10-08 22:50:51 -0400

    Update NavBar.jsx

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,53 +1,54 @@
- // eslint-disable-next-line no-unused-vars
+// eslint-disable-next-line no-unused-vars
 import React from "react";
+import { Link } from "react-router-dom"; // Importa Link
 import profileImage from '../images/javap_logo.png';
 import "../css/NavBar.css"
 
 const NavBar = () => {
-return (
+  return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-    <div className="container">
-    <a className="navbar-brand" href="/">
-        <img src={profileImage} alt="Profile" className="javap_logo" />
-    </a>
-    <button
-        className="navbar-toggler"
-        type="button"
-        data-bs-toggle="collapse"
-        data-bs-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-    >
-        <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-        <ul className="navbar-nav ms-auto">
-        <li className="nav-item">
-            <a className="nav-link" href="aboutMe">
-            About Me
-            </a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link" href="portfolio">
-            Portfolio
-            </a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link" href="skills">
-            Skills
-            </a>
-        </li>
-        <li className="nav-item">
-            <a className="nav-link" href="contact">
-            Contact
-            </a>
-        </li>
-        </ul>
-    </div>
-    </div>
+      <div className="container">
+        <Link className="navbar-brand" to="/">
+          <img src={profileImage} alt="Profile" className="javap_logo" />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/aboutMe"> {/* Cambia a Link */}
+                About Me
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/portfolio"> {/* Cambia a Link */}
+                Portfolio
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/skills"> {/* Cambia a Link */}
+                Skills
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/contact"> {/* Cambia a Link */}
+                Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
     </nav>
-    );
+  );
 };
-    
+
 export default NavBar;
