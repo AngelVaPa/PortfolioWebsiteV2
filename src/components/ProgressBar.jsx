aa2f79ee8bdf18a93ffe33954b569b56b03b9b63
Jesus Vazquez <vapa.jesusangel@gmail.com>
2024-10-08 22:22:45 -0400

    Create ProgressBar.jsx

---
+++ src/components/ProgressBar.jsx
@@ -0,0 +1,33 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useEffect, useRef } from 'react';
+import '../css/ProgressBar.css'; // Asegúrate de crear este archivo de estilo
+
+// eslint-disable-next-line react/prop-types
+const ProgressBar = ({ percentage }) => {
+  const barRef = useRef(null);
+
+  useEffect(() => {
+    let startColor = [255, 165, 0]; // Color inicial (naranja)
+    let endColor = [0, 128, 255]; // Color final (azul)
+    const interval = setInterval(() => {
+      const colorChange = startColor.map((start, index) =>
+        Math.floor(start + (endColor[index] - start) * (Math.random() * 0.05))
+      );
+      if (barRef.current) {
+        barRef.current.style.backgroundColor = `rgb(${colorChange.join(',')})`;
+      }
+    }, 100);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="progress-container">
+      <div className="progress-bar" ref={barRef} style={{ width: `${percentage}%` }}>
+        <span className="progress-text">{percentage}%</span>
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
