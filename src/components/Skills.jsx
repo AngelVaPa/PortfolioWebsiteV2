416c1c5981088696ed2dfe8520845539257e0bee
Jesus Vazquez <vapa.jesusangel@gmail.com>
2024-10-09 02:09:45 -0400

    Update Skills.jsx

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -10,7 +10,7 @@ const Skills = () => {
   const [visibleSections, setVisibleSections] = useState({});
   
   // eslint-disable-next-line no-undef
-  const apiUrl = process.env.REACT_APP_API_URL || 'https://portfoliowebsitev2-api.onrender.com/api/';
+  const apiUrl = 'https://portfoliowebsitev2-api.onrender.com/api/skills';
 
   useEffect(() => {
     const fetchSkills = async () => {
