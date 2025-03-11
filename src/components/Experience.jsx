a0e31b2dc8f73538a00ad6c1e766a120867a27c5
Jesus Vazquez <vapa.jesusangel@gmail.com>
2025-03-11 11:07:47 -0400

    add Easy Tasks

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,7 +10,7 @@ const Experience = () => {
         <h1 className="experienceTitle">Experience</h1>
         <h2>Software Development Graduate | Class of 2023</h2>
         <p className="experience-cont">Since the beginning of my studies in software development, I have actively engaged in various projects and hands-on exercises to enhance my skills. These include developing bots for Discord channels, building facial recognition attendance systems, designing and managing APIs, creating different types of websites, among others.</p>
-        <h2>Software Development Intern | DevExpress</h2>
+        <h2>Junior Software Developer | DevExpress</h2>
         <p className="experience-cont">As part of the cross-platform development team (Web, iOS, Android) at DevExpress, I contributed primarily to frontend development using Flutter and packages like Riverpod, while focusing more on backend development with Firebase and TypeScript.
         One of my key contributions to a digital inspection registration application was implementing the invitation system for associated members, leveraging the Firestore Send Email extension. Additionally, I was responsible for fully implementing the notification system using Firebase Cloud Messaging (FCM).</p>
         <h2>Nowdays</h2>
@@ -24,6 +24,10 @@ const Experience = () => {
           <div className="project">
             <a id="techBooksV1" href="https://github.com/JesusVaPa/TechBooksV1/" target="_blank" rel='noreferrer' className="thumbnail btn btn-outline-light btn-lg"></a>
             <p className='prj-title'>TechBooks V1</p>
+          </div>
+          <div className="project">
+            <a id="easyTasks" href="https://easyTasks.angelvapa.com/" target="_blank" rel='noreferrer' className="thumbnail btn btn-outline-light btn-lg"></a>
+            <p className='prj-title'>Easy Tasks</p>
           </div>
         </div>
       </div>
